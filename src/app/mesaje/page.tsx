20d703c595759ab3e2ba94adefd6a508f06b9990
Alex <cota8091@gmail.com>
2025-05-09 06:58:47 +0300

    centered the main from the new page

--- src/app/mesaje/page.tsx
+++ src/app/mesaje/page.tsx
@@ -51,7 +51,7 @@ const Mesaje = () => {
       <div>
         <Header />
         <main
-          className={`${styles.productContainer} flex justify-center items-center gap-2 min-w-container-300 w-full max-w-container-600 min-h-container-600 h-[87dvh] px-32P`}
+          className={`${styles.productContainer} flex justify-center items-center gap-2 min-w-container-300 w-full max-w-container-600 min-h-container-600 h-[87dvh] mx-auto px-32P`}
         >
           <h1 className="text-xl text-center text-white font-bold uppercase mb-32M ">
             Trimite-i mesaj owner-ului 💬
@@ -59,13 +59,13 @@ const Mesaje = () => {
           <input
             className="w-full p-2 rounded bg-gray-800 text-white border border-gray-600 appearance-none focus:outline-2 focus:outline-offset-2 focus:border-blue-600 focus:outline-solid focus:shadow-outline"
             type="email"
-            placeholder="Your email"
+            placeholder="Introduce-ti emailul"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <textarea
             className="w-full p-2 rounded bg-gray-800 text-white border border-gray-600 appearance-none focus:outline-2 focus:outline-offset-2 focus:border-blue-600 focus:outline-solid focus:shadow-outline"
-            placeholder="Your message"
+            placeholder="Mesajul tau"
             rows={5}
             value={content}
             onChange={(e) => setContent(e.target.value)}
